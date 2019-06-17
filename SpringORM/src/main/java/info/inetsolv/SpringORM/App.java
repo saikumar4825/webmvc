fcf7ee7486c74af467b5e930eb679e83026ec1e1
saikumar <saikumar@LAPTOP-UKKSA0EQ>
2019-06-17 18:06:57 +0530

    bye message added!!

--- SpringORM/src/main/java/info/inetsolv/SpringORM/App.java
+++ SpringORM/src/main/java/info/inetsolv/SpringORM/App.java
@@ -3,22 +3,21 @@ package info.inetsolv.SpringORM;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.orm.hibernate5.HibernateTemplate;
 
-
 public class App {
 
-public static void main(String[] args) {
+	public static void main(String[] args) {
 
-	ClassPathXmlApplicationContext container = new ClassPathXmlApplicationContext("spring.xml");
-	
- HibernateTemplate hibernateTemplate = container.getBean(HibernateTemplate.class);
- 	Employee employee = new Employee();
+		ClassPathXmlApplicationContext container = new ClassPathXmlApplicationContext("spring.xml");
+
+		HibernateTemplate hibernateTemplate = container.getBean(HibernateTemplate.class);
+		Employee employee = new Employee();
 // Employee employee = hibernateTemplate.load(Employee.class, 1);
-  hibernateTemplate.load(employee, 1); 
-	
-	System.out.println(employee.getEno());
-	System.out.println(employee.getName());
-	System.out.println(employee.getSalary());
+		hibernateTemplate.load(employee, 1);
 
-	container.close();
-}
+		System.out.println(employee.getEno());
+		System.out.println(employee.getName());
+		System.out.println(employee.getSalary());
+		System.out.println("bye");
+		container.close();
+	}
 }
